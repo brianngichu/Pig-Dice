4b2e8b97b3a660bfe0de655329cdcec464029139
Ngichu <brianngichu@gmail.com>
2019-05-13 01:59:10 +0300

    created a contructor for scores

--- js/scripts.js
+++ js/scripts.js
@@ -4,3 +4,10 @@ var player2="";
 var throwdice = function () {
     return Math.floor(6 * Math.random())+1;
 }
+function Player(turn) {
+    this.roll = 0;
+    this.rollscore = 0;
+    this.totalscore = 0;
+    this.turn = turn;
+    this.playerName;
+}
